0a78bff62d4210de7c297f88903d60a9e4a01077
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-02-18 20:35:58 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,6 +4,7 @@
 def meal_choice(veg1, veg2, protein = "meat")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  return "A plate of #{protein} with #{veg1} and #{veg2}."
 end 
 
 # #2020.02.14.JP: Question: How do I give this method an explicit return value? (so that the return value includes the three food items I've chosen?) 
